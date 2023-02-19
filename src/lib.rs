913b83d056fae0e316b914ca1ebd3bc814072e85
hunterdw0007 <hunterw.mail@gmail.com>
2023-02-18 20:52:36 -0600

    feat: impl for new, create, and insert

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,46 @@
-use std::{rc::Rc, cell::RefCell};
 
 #[derive(Debug)]
-struct BinaryTree {
-    pub value: u128,
-    pub first_child: Option<BinaryTreeRef>,
-    pub second_child: Option<BinaryTreeRef>,
+#[derive(PartialEq)]
+pub enum CollatzTree {
+    Leaf {
+        value: u128,
+        odd: Box<CollatzTree>,
+        even: Box<CollatzTree>,
+    },
+    Empty,
 }
 
-type BinaryTreeRef = Rc<RefCell<BinaryTree>>;
+impl CollatzTree {
+    pub fn new() -> Self {
+        CollatzTree::Empty
+    }
 
-impl BinaryTree {
-    /// Return an Option reference to the BinaryTree child of this BinaryTree that
-    /// contains val or None if is not anywhere in the BinaryTree 
-    pub fn find(&self, val:&u128) {
-       
+    pub fn create(val: u128) -> Self {
+        CollatzTree::Leaf { 
+            value: val, 
+            odd: Box::new(CollatzTree::Empty), 
+            even: Box::new(CollatzTree::Empty) 
+        }
+    }
+    pub fn insert(&mut self, new_value: u128) {
+        match self {
+            CollatzTree::Leaf { 
+                ref value, 
+                ref mut odd, 
+                ref mut even 
+            } => {
+                if *value == new_value {
+                        return;
+                }
+                match new_value % 2 {
+                    0 => even.insert(new_value),
+                    1 => odd.insert(new_value),
+                    _ => panic!()
+                }
+            },
+            CollatzTree::Empty => {
+                *self = CollatzTree::create(new_value);
+            }
+        }
     }
 }
