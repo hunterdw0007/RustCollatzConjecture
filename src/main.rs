9c786b9b53ca36151de04f59fd06e3a43c49b4dc
hunterdw0007 <hunterw.mail@gmail.com>
2023-02-21 20:48:05 -0600

    feat: collatz function in main, work on inserting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-use std::collections::HashSet;
-
 use collatz::CollatzTree;
 
 fn main() {
     println!("Following the numbers in the Collatz Conjecture:");
 
-    let mut tree = CollatzTree::new();
+    let mut tree = CollatzTree::create(1);
 
-    for i in 0..10 {
-        tree.insert(i);
+    let mut numbers: Vec<u128>;
+
+    for i in 1..11 {
+        numbers = collatz(i);
+
     }
 
     CollatzTree::print(&tree, 0);
+}
+
+pub fn collatz(start: u128) -> Vec<u128> {
+    let mut value = start;
+    let mut output = vec![start];
+
+    while value != 1 {
+        
+        if value % 2 == 0 {
+            value = value / 2;
+        }
+        else {
+            value = 3 * value + 1;
+        }
+        
+        output.push(value);
+    }
+    output
+}
+
+pub fn insert_at_prev(tree: &mut CollatzTree, numbers: &mut Vec<u128>) {
+    let mut prev = numbers.pop();
+
+    for x in numbers {
+        unimplemented!();
+        //get a tree with its root as prev
+        //insert x to that tree
+    }
 }
